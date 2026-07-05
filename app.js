cb15dd7df9acb4c5bef39a8fe9fa01a8b1c6915e
eashwarpolishetti <polishettieashwar04@gmail.com>
2026-07-05 17:02:33 +0530

    project is completed and all files are up-to-date

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ let CompScore=0;
 const choices=document.querySelectorAll(".choice");
 const msg=document.querySelector("#msg-cont");
 
+const userScoreLine=document.querySelector("#user-score");
+const compScoreLine=document.querySelector("#computer-score");
+
 // generating computer choice
 const Gencomchoice=()=>{
     const listofChoices=['rock','paper','scissor'];
@@ -13,30 +16,40 @@ const Gencomchoice=()=>{
 
 
 
-const draw=(()=>{
-    console.log("it's a draw match");
-});
+const draw = () => {
+    msg.innerText = "It's a draw match! 🤝";
+    msg.style.backgroundColor = "#f1f5f9"; // Neutral gray background
+    msg.style.borderColor = "#64748b";
+};
 
-let showWinner=((userWin)=>{
+let showWinner=(userWin,userchoice,compchoice)=>{
     if(userWin){
-        msg.innerText="you won";
+        UserScore++;
+        userScoreLine.innerText=UserScore;
+        msg.innerText= `You win  🎉 Your ${userchoice} beats ${compchoice}.`;
         // console.log("you won marraa");
+        msg.style.backgroundColor = "#bbf7d0";
+        msg.style.borderColor = "#22c55e";
     }
     else{
-        // console.log("Hard luck");
-        msg.innerText="Sorry you lost! better luck next time";
+        CompScore++;
+        compScoreLine.innerText=CompScore;
+        // console.log("you lost");
+        msg.innerText= `You lose 🫠 ${compchoice} beats your ${userchoice}.`;
+        msg.style.backgroundColor = "#fecaca";
+        msg.style.borderColor = "#ef4444";
     }
-});
+};
 
 const playgame=(userchoice)=>{ 
     const compchoice=Gencomchoice();
-    console.log("userchoice ",userchoice);
-    console.log("computer choice is ",compchoice);
+    // console.log("userchoice ",userchoice);
+    // console.log("computer choice is ",compchoice);
     if(userchoice===compchoice)
     {
         // console.log("it's a draw");
         draw();
-        msg.innerText="it's a draw match";
+        // msg.innerText="it's a draw match";
     }
     else{
         let userWin=true;
@@ -49,14 +62,14 @@ const playgame=(userchoice)=>{
         else{
             userWin=compchoice==="rock"?false:true;
         }
-        showWinner(userWin);
+        showWinner(userWin,userchoice,compchoice);
     }
-}
+};
 
 choices.forEach((choice)=>{
     choice.addEventListener('click',()=>{
         const userchoice=choice.getAttribute("id");
         playgame(userchoice);
-        console.log("choice was clicked");
+        // console.log("choice was clicked");
     });
 });
